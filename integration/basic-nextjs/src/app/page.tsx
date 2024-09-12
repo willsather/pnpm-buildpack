5721b96e75491bbd9fa353bf03e2f774ede0b447
Will Sather <satherw@vmware.com>
2024-09-11 21:09:24 -0400

    chore: add basic nextjs app

---
+++ integration/basic-nextjs/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="my-8 font-bold">Basic Next.js</h1>
+    </main>
+  );
+}
